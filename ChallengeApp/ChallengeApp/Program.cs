0df3289d4e2e8175993e35491acc8b309221f732
Librix2 <librix2@protonmail.com>
2023-12-02 15:30:28 +0100

    Instrukcja If i podstawy debugowania

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿  //Instrukcja If i podstawy debugowania
+
+// Operatory relacyjne
+// == != < > <= >=
+
+var number1 = 50;
+var number2 = 10;
+if(number1 < number2)
+{
+    Console.WriteLine("Jestem TUTAJ w LINII 39");
+}
+else
+{
+    Console.WriteLine("Jestem TUTAJ w LINII 43");
+}
